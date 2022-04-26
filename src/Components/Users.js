090bc9fd4cda31bfd5e039e558fa55c10250ee7d
stanleyh1 <hannahstanley63@gmail.com>
2022-04-26 15:15:36 +0100

    makes progress with getUsers functionality

--- src/Components/Users.js
+++ src/Components/Users.js
@@ -1,30 +1,45 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { userContext } from '../utils/user';
-import jessjelly from '../images/jessjelly.jpeg';
-import amy from '../images/amypoehler.jpeg';
-import grumpy from '../images/grumpy.jpeg';
-import weegem from '../images/weegem.jpeg'
+import { getUsers } from '../utils/api'
+
+// import jessjelly from '../images/jessjelly.jpeg';
+// import amy from '../images/amypoehler.jpeg';
+// import grumpy from '../images/grumpy.jpeg';
+// import weegem from '../images/weegem.jpeg'
 
 const Users = () => {
 
-const users = [
-    {username:'weegembump', name:'Gemma Weeg', avatar_url: weegem},
-    { username: 'happyamy2016', name : 'Amy Coder', avatar_url: amy},
-    {username: 'jessjelly', name : 'Jess LovesCode', avatar_url: jessjelly},
-    {username: 'grumpy19', name : 'Mr Grumpy', avatar_url: grumpy}
-    ]
+    const [users, setUsers] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        getUsers().then((users) => {
+            setUsers(users)
+            setIsLoading(false)
+        })
+    }, [])
+
+// const users = [
+//     {username:'weegembump', name:'Gemma Weeg', avatar_url: weegem},
+//     { username: 'happyamy2016', name : 'Amy Coder', avatar_url: amy},
+//     {username: 'jessjelly', name : 'Jess LovesCode', avatar_url: jessjelly},
+//     {username: 'grumpy19', name : 'Mr Grumpy', avatar_url: grumpy}
+//     ]
 
 const { setLoggedInUser } = useContext(userContext)
 
 const logIn = (newUser) => {
     setLoggedInUser(newUser)
 }
+
+if (isLoading)  {
+    return <p>Loading...</p>
+}
     return (
     <div>
         <h2>Users of Ribbit</h2>
         <ul className='users'>
         {users.map((user) => {
-            console.log(user)
             return (
             <li className='user' key={user.username}>
                 <div className="username_name_container">
